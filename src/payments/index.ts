4b01b63111ca4033935159c76bfda79864c4154e
Nigel Nindo <nigelnindodev@gmail.com>
2023-05-06 16:45:37 +0300

    Fix controller referencing bug

--- src/payments/index.ts
+++ src/payments/index.ts
@@ -1,6 +1,13 @@
-import {BudPayProcessingRequest} from "../constants";
+import {BudPayProcessingRequest, SupportedCurrencies} from "../constants";
 
+import { v4 as uuidv4 } from 'uuid';
 
 export async function processBudPayPaymentRequest(amount: string, email: string) {
-
+    const payload: BudPayProcessingRequest = {
+      amount,
+      email,
+      currency: SupportedCurrencies.NIGERIA, // We will only use Nigeria for now
+      reference: uuidv4(),
+      callback: "www.budpay.com" // TODO: Implement callback functionality
+    };
 }
